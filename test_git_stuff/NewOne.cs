8c37b21ee2c9aab5377d39c8888901626bdd1ade
4Headd <anikitin2508@mail.ru>
2022-01-29 17:21:41 +0300

    create newClass

---
+++ test_git_stuff/NewOne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_git_stuff
+{
+    class NewOne
+    {
+        public int a = 42;
+    }
+}
